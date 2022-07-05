a1fe236b91842c9cfe50c53bc16b892ff7c4423b
Mario <mariopolonia0@gmail.com>
2022-07-05 07:43:47 -0400

    Testing a la UI

--- app/src/androidTest/java/com/duramas_security/ocupacionescompose/componen/BarraTituloKtTest.kt
+++ app/src/androidTest/java/com/duramas_security/ocupacionescompose/componen/BarraTituloKtTest.kt
@@ -1,11 +1,15 @@
 package com.duramas_security.ocupacionescompose.componen
 
+import androidx.compose.ui.test.assertIsSelected
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
 import org.junit.Rule
 import org.junit.Test
 
-
+//este test es a la barra de titulo
+//el rule
 class BarraTituloKtTest{
+    //el createComposeRule permite establecer el contenido de Compose
     @get:Rule
     val composeTestRule = createComposeRule()
 
@@ -17,6 +21,9 @@ class BarraTituloKtTest{
                 _counter = 6
             )
         }
-        Thread.sleep(5000)
+        //Thread.sleep(5000)
+        composeTestRule.onNodeWithContentDescription("ya si enel").assertIsSelected()
     }
+
+
 }
